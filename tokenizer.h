91c44d2432b08fab17edf3005db0016282e5d909
madhav-gullapalli <89715988+madhav-gullapalli@users.noreply.github.com>
2026-06-08 20:47:01 -0400

    Add CP++ tokenizer interface

---
+++ tokenizer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+enum class TokenKind {
+    Identifier,
+    Integer,
+    Float,
+    String,
+    Char,
+    LeftParen,
+    RightParen,
+    Comma,
+    Semicolon,
+    Equals,
+    Operator,
+    Unknown,
+    EndOfFile
+};
+
+struct SourceSpan {
+    int startLine;
+    int startColumn;
+    int endLine;
+    int endColumn;
+};
+
+struct Token {
+    TokenKind kind;
+    std::string text;
+    SourceSpan span;
+};
+
+std::vector<Token> tokenize(const std::string& source);
+std::string tokenKindName(TokenKind kind);
